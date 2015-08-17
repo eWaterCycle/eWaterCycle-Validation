969b80be1251280e0a27d799012d7e308dd62d58
Rolf Hut <rwhut@Rolfs-TUDelft-MacBook-Pro.local>
2015-08-17 15:49:31 +0200

    first working version, using fake data

--- eWaterCycleValidate.sh
+++ eWaterCycleValidate.sh
@@ -5,27 +5,32 @@
 #This script heavily uses the CDO library, make sure you have it installed, with grib
 # and netCDF support. 
 
+GRDCLocation=../../localData/GRDC/2stationsDaily/
+#here the ensemble is stored, the entire ensemble!
+eWaterOutput=../../localData/sampleOutput/ 
+targetDay=2015-08-14T00:00:00
+#targetday + end-forecast
+endForcastDay=2015-08-22T00:00:00
+outputFolder=../../localData/sampleOutput/
 
-eWaterOutput= ../../localData/tmp/someOutputTest.nc
-targetDay=YYYY-MM-DDThh:mm:ss
-endForcastDay=YYYY-MM-DDThh:mm:ss #targetday + end-forecast
+#will be cleaned at end of script
+tmpFolder=../../localData/tmp 
 
 #first, convert GRDC data to NetCDF. This only needs to be done once. See the seperate
 #repo at https://github.com/RolfHut/GRDC2NetCDF
-python ../GRDC2NetCDF/GRDC2NetCDF.py ../../localData/GRDC/2stationsDaily/ daily ../../localData/tmp/testDaily.nc 1990-01-01 2000-01-01
+echo python ../GRDC2NetCDF/GRDC2NetCDF.py ${GRDCLocation} daily ${tmpFolder}/testDailyOld.nc 1990-01-01 2000-01-01
+
+#THIS IS ONLY BECAUSE I DO NOT HAVE THE GRDC DATA YET AND MAKES NO HYDROLOGICAL SENSE
+echo cdo shifttime,20year ${tmpFolder}/testDailyOld.nc ${tmpFolder}/testDaily.nc
 
 #select one forecast-day. doing in this 2 steps, so this can be put into a loop later on.
-cdo seldate,${targetDay},${endForcastDay} ../../localData/tmp/testDaily.nc ../../localData/tmp/tmp/GRDCSelection.nc
+echo cdo seldate,${targetDay},${endForcastDay} ${tmpFolder}/testDaily.nc ${tmpFolder}/GRDCSelection.nc
 
 #interpolate output eWaterCycle (a grid) to GRDC station locations (points)
-for ensembleMember in {01..20..1}
+for ensembleMember in {01..05..1}
 do
-    cdo remapnn,../../localData/tmp/testDaily.nc ${eWaterOutput}/FOOBARENSEMBLESTRUCTUUR ../../localData/tmp/tmp/eWaterCycleGRDCMap.nc
+    echo cdo remapnn,${tmpFolder}/testDaily.nc ${eWaterOutput}/work${ensembleMember}/output/netcdf/discharge_dailyTot_output.nc ${tmpFolder}/eWaterCycleGRDCMap${ensembleMember}.nc
 done
 
 #calculate Brier scores
-ensbrs,0.95 ../../localData/tmp/tmp/GRDCSelection.nc ${eWaterOutput} ../../localData/tmp/result
-
-exit
-
-
+cdo ensbrs,0.95 ${tmpFolder}/GRDCSelection.nc ${tmpFolder}/eWaterCycleGRDCMap*.nc ${outputFolder}/validation
